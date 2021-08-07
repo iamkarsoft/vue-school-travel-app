4a46da9bd7d601f3af1ab74fbc46e2e9d898052d
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-08-07 11:32:57 +0000

    Go back feature

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <GoBack />
   <section class="destination">
     <h1>{{ destination.name }}</h1>
     <div class="destination-details">
@@ -33,8 +34,12 @@
 
 <script>
 import store from "@/store.js";
+import GoBack from "@/components/GoBack"
 export default {
   name: "DestinationDetails",
+  components:{
+    GoBack,
+  },
   props:{
     slug:{
       type: String,
